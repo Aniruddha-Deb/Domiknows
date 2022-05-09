ced77dc39a4cb8f686f7e8c1762d8d634c2969c5
Aniruddha-Deb <aniruddha.deb.2002@gmail.com>
2022-05-09 21:02:39 +0530

    Issa mess

--- domino.py
+++ domino.py
@@ -8,10 +8,14 @@ import matplotlib.pyplot as plt
 class Domino:
 
     def __init__(self, a, b):
-        self.d = (a,b)
+        if a < 0 or b < 0:
+            raise ValueError("Pips cannot have negative values")
+        if a > (1<<32) or b > (1<<32):
+            raise ValueError("Pip values cannot be larger than 2^31")
+        self._d = (a,b)
 
     def __str__(self):
-        return f"[{self.d[0]}|{self.d[1]}]"
+        return f"[{self._d[0]}|{self._d[1]}]"
 
     def __eq__(self,other):
         if self is None and other is None:
@@ -19,50 +23,207 @@ class Domino:
         elif (self is None and other is not None) or (other is None and self is not None):
             return False
         else:
-            return self.d == other.d or self.d == other.d[::-1]
+            return self._d == other._d or self._d == other._d[::-1]
 
     def __getitem__(self,key):
-        return self.d[key]
+        return self._d[key]
 
     def __contains__(self, item):
-        return item in self.d
+        return item in self._d
 
     def __repr__(self):
         return self.__str__()
 
     def __iter__(self):
-        return self.d.__iter__()
+        return self._d.__iter__()
 
     def __hash__(self):
-        return self.d.__hash__()
+        return (max(self._d)<<32) | min(self._d)
 
-    def join(self,other):
+    def connections(self,other):
         j = []
-        if self.d[0] == other.d[0] or self.d[0] == other.d[1]:
-            j.append(self.d[0])
-        if self.d[1] == other.d[0] or self.d[1] == other.d[1]:
-            j.append(self.d[1])
+        if self._d[0] == other._d[0] or self._d[0] == other._d[1]:
+            j.append(self._d[0])
+        if self._d[1] == other._d[0] or self._d[1] == other._d[1]:
+            j.append(self._d[1])
         return j
 
-    def exclusion(l1,l2):
-        return [p for p in l1 if p not in l2]
-
-    def union(l1, l2):
-        a = exclusion(l1,l2)
-        a += l2
-        return a
-
-    def intersection(l1, l2):
-        return [p for p in l1 if p in l2]
-
     def is_double(self):
-        return self.d[0] == self.d[1]
+        return self._d[0] == self._d[1]
 
     def flatten(pips):
-        return set([i for p in pips for i in p.d])
+        return set([i for p in pips for i in p._d])
 
     def sum(pips):
         return sum(sum(h) for h in pips)
+
+class Board:
+
+    def __init__(self, has_spinner=False):
+        self.data = {}
+        self.spatial_data = {}
+        self.has_spinner = has_spinner
+        self.spinner = None
+
+    def get_terminals(self):
+        terminals = {}
+        for bone in self.data:
+            if len(self.data[bone]) < 2:
+                terminals.add(bone)
+            elif bone == self.spinner and len(self.data[bone]) < 4:
+                terminals.add(bone)
+        return terminals
+
+    def get_all_connections(self):
+        terminals = self.get_terminals()
+        conns = {}
+        for p in terminals:
+            if p.is_double():
+                conns.add(p[0])
+            else:
+                conns.union(set((p[0],p[1])).difference(Domino.flatten(self.data[p])))
+        return conns
+
+    def get_connections_for(self, pip):
+        if pip == spinner and len(self.data[pip]) < 4:
+            conns = (4-len(self.data[pip]))*[pip[0]]
+        elif pip.is_double() and len(self.data[pip]) < 2:
+            conns = (2-len(self.data[pip]))*[pip[0]]
+        else:
+            conns = list(set())
+
+
+
+    def free_end(self, pip):
+        return set((p[0],p[1])).difference(Domino.flatten(self.data[p])).pop()
+
+
+    #  origin  TT
+    #        +───┐
+    #     LT │[0]│ RT
+    #     LL ├───┤ RR
+    #     LB │[1]│ RB
+    #        └───┘
+    #          BB
+    # 
+    # rotation is measured anticlockwise w.r.t origin
+    # [0] -> number at idx 0 of pip, similarly for [1]
+
+    def connect(self, tgt, src):
+        if not src:
+            raise ValueError("src cannot be None")
+        if src in self.data:
+            raise ValueError("src already exists in board")
+
+        if tgt == None and self.data == {}:
+            # first move
+            self.data[src] = {}
+            self.spatial_data[src] = {
+                'pos': (-0.5,1),
+                'rot': 0 if src.is_double() else 90,
+                'conns': {}
+            }
+        elif tgt not in self.data:
+            raise ValueError("tgt does not exist in board")
+        elif (len(self.data[tgt]) == 2 and tgt != self.spinner):
+            raise ValueError("tgt is already connected to two other pips")
+        elif (len(self.data[tgt]) == 4 and tgt == self.spinner):
+            raise ValueError("tgt as spinner is already connected to four other pips")
+        elif not tgt.connections(src):
+            raise ValueError("tgt and src don't have any connections possible")
+        else:
+            self.data[tgt].add(src)
+            self.data[src] = {tgt}
+
+            tgtx = self.spatial_data[tgt]['pos'][0]
+            tgty = self.spatial_data[tgt]['pos'][1]
+            tgtr = self.spatial_data[tgt]['rot']
+
+            if len(self.data[tgt]) > 2:
+                # tgt is a double and a spinner.
+                if "TT" not in self.spatial_data[tgt]['conns']:
+                    self.spatial_data[tgt]['conns'].add("TT")
+                else:
+                    self.spatial_data[tgt]['conns'].add("BB")
+
+            elif tgt.is_double():
+                if "LL" not in self.spatial_data[tgt]['conns']:
+                    # connect to LL
+                    self.spatial_data[src] = {
+                        'pos': 
+                        'rot': 0 if src.is_double() else 90,
+                        'conns': {} 
+                    }
+                    self.spatial_data[tgt]['conns'].add("LL")
+                else:
+
+            else:
+                # normal pip connect
+                tgtx = 
+                if tgt[0] == src[0]:
+                    if tgtr == 0:
+                        self.spatial_data[src] = {
+                            'pos': (tgtx+1, tgty),
+                            'rot': 180,
+                            'conns': {'TT'}
+                        }
+
+
+    def print_board(self):
+        # large print v/s small print. Large print only!
+        # convert the board to a chain
+        if not self.board:
+            print("[]")
+            return
+
+        # have a curses-like array that you can blit a domino onto at a point
+        w = 50
+        h = 50
+        text_arr = h*[w*[' ']]
+
+        board_arr = []
+        curr_pips = deque()
+        first_key = list(self.board.keys())[0]
+        board_arr.append(first_key)
+        curr_pips.append(first_key)
+        
+        while curr_pips:
+            key = curr_pips.popleft()
+            key_conns = self.board[key]
+            for k in key_conns:
+                idx = board_arr.index(key)
+                kip = board_arr[board_arr.index(key)]
+                if k not in board_arr:
+                    curr_pips.append(k)
+                    if idx == 0:
+                        board_arr.insert(0, k)
+                    else:
+                        board_arr.append(k)
+
+        # if there's a spinner, construct a top-down array as well.
+        while curr_pips:
+
+
+        print(f"BOARD: ", end="")
+        prev_inv = False
+        for i in range(len(board_arr)-1):
+            c,n = board_arr[i],board_arr[i+1]
+            if c[1] == n[0] or c[1] == n[1]:
+                print(board_arr[i],end="")
+                prev_inv = False
+            else:
+                print(f"[{board_arr[i][1]}|{board_arr[i][0]}]", end="")
+                prev_inv = True
+
+        if len(board_arr) == 1:
+            print(board_arr[0])
+        else:
+            if (not prev_inv and board_arr[-1][0] == board_arr[-2][1]) or \
+               (prev_inv and board_arr[-1][0] == board_arr[-2][0]):
+                print(board_arr[-1])
+            else:
+                print(f"[{board_arr[-1][1]}|{board_arr[-1][0]}]")
+
 
 class Board:
 
@@ -298,7 +459,7 @@ class BlockGame:
     def validate_move(board, tgt, src):
         if (not tgt and board.board == {}):
             return True
-        intersecn = tgt.join(src)
+        intersecn = tgt.connections(src)
         if len(intersecn) < 1:
             return False
         return (tgt in board.board \
@@ -331,7 +492,7 @@ class BlockGame:
 
     def play_board(self):
 
-        self.board = Board.deal_board(self.n_players, self.n_pips, self.pipset_size)
+        self.board = Board._deal_board(self.n_players, self.n_pips, self.pipset_size)
         self.board.curr_player = self.arbiter(self.board)
 
         while not (min(map(lambda x: len(x),self.board.hands)) == 0 or self.blocked(self.board)):
@@ -341,14 +502,14 @@ class BlockGame:
             if BlockGame.matches(connections, self.board.hands[curr_player]):
                 (tgt, src) = self.player_moves[curr_player](self.board.hidden_copy(curr_player))
                 if BlockGame.validate_move(self.board, tgt, src):
-                    board.do_move((tgt,src),(tgt,src))
+                    board._do_move((tgt,src),(tgt,src))
                 else:
                     print((tgt,src))
-                    self.board.debug_board()
+                    self.board._debug_board()
                     raise Exception("Invalid Move")
             else:
                 print(f"Player {curr_player+1} Blocked!")
-                board.do_move("BLOCK",None)
+                board._do_move("BLOCK",None)
 
         # What do we do if two (or more) people tie on minimum scores? 
         # A: split the rest of the sum between the two
@@ -382,7 +543,7 @@ def user_move(board):
     if (board.board == {}):
         in_str = input("<src>: ")
         if (in_str == "d"):
-            board.debug_board()
+            board._debug_board()
             return user_move(board)
         captures = re.match("\(([0-9]),([0-9])\)", in_str)
         D = tuple(int(i) for i in captures.groups())
@@ -390,7 +551,7 @@ def user_move(board):
     else:
         in_str = input("<tgt> <src>: ")
         if (in_str == "d"):
-            board.debug_board()
+            board._debug_board()
             return user_move(board)
         captures = re.match("\(([0-9]),([0-9])\)\\s*\(([0-9]),([0-9])\)",in_str)
         vals = [int(i) for i in captures.groups()]
@@ -511,14 +672,14 @@ def EMM_eval_pos(board,ilvl,alpha,beta):
         valid_moves = BlockGame.valid_moves(board)
         if not valid_moves:
             # blocked!
-            board.do_move("BLOCK",None)
+            board._do_move("BLOCK",None)
             val = EMM_eval_pos(board,ilvl+1,alpha,beta)
             board.undo_move()
             return val
         else:
             val = -math.inf
             for move in valid_moves:
-                board.do_move(move,move)
+                board._do_move(move,move)
 
                 val = max(val,EMM_eval_pos(board,ilvl+1,alpha,beta))
                 board.undo_move()
@@ -542,7 +703,7 @@ def EMM_eval_pos(board,ilvl,alpha,beta):
 
         if vm == 0:
             # unconditionally blocked. 
-            board.do_move("BLOCK",None)
+            board._do_move("BLOCK",None)
             val = EMM_eval_pos(board,ilvl+1,alpha,beta)
             board.undo_move()
             return val
@@ -559,7 +720,7 @@ def EMM_eval_pos(board,ilvl,alpha,beta):
         P_block = math.comb(n-vp,r)/math.comb(n,r)
         val_accum = 0
         for move in valid_moves:
-            board.do_move(move,move)
+            board._do_move(move,move)
 
             val_accum += EMM_eval_pos(board,ilvl+1,alpha,beta)
 
@@ -586,7 +747,7 @@ def computer_move_EMM(board):
         i = 0
         idx = -1
         for move in valid_moves:
-            board.do_move(move,move)
+            board._do_move(move,move)
 
             nval = EMM_eval_pos(board,1,math.inf,-math.inf)
             if nval > val:
@@ -598,7 +759,7 @@ def computer_move_EMM(board):
 
         return valid_moves[idx]
     except ValueError:
-        board.debug_board()
+        board._debug_board()
 
 def get_valid_pips(invis_pips, blocked_nos):
     ret_pips = []
@@ -613,12 +774,64 @@ def get_valid_pips(invis_pips, blocked_nos):
 # I'll start off with 2, see if it's fast and simple, then try implementing 1
 # (with the improvements described in it)
 def solve_PI(board, player):
-    # Hmmmm.... How do we solve a perfect information board?
-    # Evaluate the gametree deterministically
-    pass
-        # maximize
+    if board.n_players != 2:
+        raise Exception("PI solving works only for two players now")
 
+    if (n_opp_pips <= 0):
+        return -Domino.sum(board.hands[player])
+    if (len(our_pips) <= 0):
+        return Domino.sum(board.hands[(player+1)%2]) # 2 player so no worries
 
+    if len(board.actions) >= 2 and board.actions[-1] == "BLOCK" and board.actions[-2] == "BLOCK":
+        # terminal node!
+        our_sum = Domino.sum(board.hands[player])
+        opp_sum = Domino.sum(board.hands[(player+1)%2])
+        if our_sum < opp_sum: 
+            # we win
+            return opp_sum
+        else:
+            return -our_sum
+
+    valid_moves = BlockGame.valid_moves(board)
+
+    if board.curr_player == player:
+        val = -math.inf
+        bmove = None
+        for move in valid_moves:
+            board._do_move(move,move)
+            nval = solve_PI(board, player)
+            board.undo_move()
+            if nval > val:
+                val = nval
+                bmove = move
+
+        if not bmove:
+            # blocked
+            board._do_move("BLOCK", None)
+            nval = solve_PI(board, player)
+            board.undo_move()
+            val = nval
+            bmove = "BLOCK"
+        else:
+    else:
+        val = math.inf
+        bmove = None
+        for move in valid_moves:
+            board._do_move(move,move)
+            nval = solve_PI(board, player)
+            board.undo_move()
+            if nval < val:
+                val, bmove = nval, move
+
+        if not bmove:
+            # blocked
+            board._do_move("BLOCK", None)
+            nval = solve_PI(board, player)
+            board.undo_move()
+            val = nval
+            bmove = "BLOCK"
+
+    return val
 
 def computer_move_PIMC(board):
     # get candidate pipset(s), given board information
@@ -636,9 +849,9 @@ def computer_move_PIMC(board):
     while move_stack:
         move = move_stack.pop()
         if move == "BLOCK":
-            board.do_move("BLOCK",None)
+            board._do_move("BLOCK",None)
         else:
-            board.do_move(move,move)
+            board._do_move(move,move)
 
     # do PIMC for 200 samples:
     moves = []
